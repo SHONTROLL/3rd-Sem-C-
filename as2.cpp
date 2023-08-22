a271ec5b8cc85e0f9d543e3e20c7c5fe0b1264fe
SHONTROLL <111211887+SHONTROLL@users.noreply.github.com>
2023-08-22 08:52:13 +0530

    Add files via upload

---
+++ as2.cpp
@@ -0,0 +1,37 @@
+//Q1 WAp in C++ to find and delete a given character from a given string.
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string str;
+	string temp;
+	char c,t;
+	cout<<"Enter a String";
+	
+		getline(cin,str);
+	
+	cout<<"Enter the character to delete";
+	
+	
+		getline(cin,c);
+	
+	for(int i;i<10;i++)
+	{
+		if(c==str[i])
+		{
+			
+			
+			
+			
+			
+			
+		}
+	}
+	cout<<"The string after deletion is:"<<str;
+
+		
+	
+	return 0;
+}
